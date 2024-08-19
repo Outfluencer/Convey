d37295a9fbc9b4eec5bb272dd72dc19de937e749
Outfluencer <realoutfluencer@gmail.com>
2024-08-20 01:16:59 +0200

    work in progress

--- bukkit/src/main/java/net/outfluencer/convey/ConveyCommand.java
+++ bukkit/src/main/java/net/outfluencer/convey/ConveyCommand.java
@@ -1,5 +1,6 @@
 package net.outfluencer.convey;
 
+import net.outfluencer.convey.protocol.packets.ServerInfoPacket;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -9,6 +10,9 @@ public class ConveyCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         sender.sendMessage(ChatColor.BLUE + "Convey by Outfluencer, current server name " + Convey.getInstance().getServerName());
+        for (ServerInfoPacket.Host server : Convey.getInstance().getServers()) {
+            sender.sendMessage(ChatColor.BLUE + " - " + server.getName() + " " + server.getAddress() + " " + (server.isRequiresPermission() ? "Requires permission" : "No permission required"));
+        }
         return false;
     }
 }
